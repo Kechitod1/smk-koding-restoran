063bf8f8aedf19a65e86b493eff931e9fda53af9
Kechitod1 <onesiforusnoe@gmail.com>
2023-12-01 11:40:09 +0700

    restoran

---
+++ ecommerce/composables/constants/category.ts
@@ -0,0 +1,14 @@
+export const category: Category[] = [
+    {
+        id: 1,
+        name: "Makanan",
+    },
+    {
+        id: 2,
+        name: "Minuman",
+    },
+]
+export interface Category {
+    id?: number,
+    name?: string,
+}
---
+++ ecommerce/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <br>
+    <br>
+    <center>
+    <h2 class="heder text-xl font-bold">Selamat Datang Di Resto</h2>
+    <img src="../public/Resto.jpg" style="width: 30%;">
+    </center>
+</template>
